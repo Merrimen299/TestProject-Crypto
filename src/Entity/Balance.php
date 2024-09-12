8f161db58572219c6f2043a90cea5206f0a52085
ipz203_bvg <ipz203_bvg@student.ztu.edu.ua>
2024-09-12 21:08:24 +0300

    first commit

---
+++ src/Entity/Balance.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BalanceRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: BalanceRepository::class)]
+class Balance
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $number = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $market = null;
+
+    #[ORM\Column]
+    private ?float $amount = null;
+
+    #[ORM\Column]
+    private ?float $price = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNumber(): ?string
+    {
+        return $this->number;
+    }
+
+    public function setNumber(string $number): static
+    {
+        $this->number = $number;
+
+        return $this;
+    }
+
+    public function getMarket(): ?string
+    {
+        return $this->market;
+    }
+
+    public function setMarket(string $market): static
+    {
+        $this->market = $market;
+
+        return $this;
+    }
+
+    public function getAmount(): ?float
+    {
+        return $this->amount;
+    }
+
+    public function setAmount(float $amount): static
+    {
+        $this->amount = $amount;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): static
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+}
